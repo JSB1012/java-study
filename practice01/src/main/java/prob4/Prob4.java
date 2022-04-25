a0dc1c7fc02da922fff89e702c8ca011caa2038d
JSB1012 <sbj15102@naver.com>
2022-04-25 22:33:28 +0900

    practice

--- practice01/src/main/java/prob4/Prob4.java
+++ practice01/src/main/java/prob4/Prob4.java
@@ -7,13 +7,8 @@ public class Prob4 {
 	public static void main(String[] args) {
 
 		/*
-		 * System.out.println(str.substring(0,1));
-		 * System.out.println(str.substring(0,2));
-		 * System.out.println(str.substring(0,3));
-		 * System.out.println(str.substring(0,4));
-		 * System.out.println(str.substring(0,5));
-		 * System.out.println(str.substring(0,6));
-		 * System.out.println(str.substring(0,7));
+		 * 문자열을 입력 받아서 아래와 같은 실행결과가 나타나도록 Prob4 클래스의 main 메쏘드를 완성 하세요. 입력 받은 문자열을 한 글자씩
+		 * 증가시키며 출력하는 프로그램입니다.
 		 */
 
 		Scanner scanner = new Scanner(System.in);
