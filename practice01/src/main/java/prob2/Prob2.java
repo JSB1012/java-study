ee27e8990b200499be94bd2f6a667c4c34a3ceb3
JSB1012 <sbj15102@naver.com>
2022-04-25 22:36:00 +0900

    practice

--- practice01/src/main/java/prob2/Prob2.java
+++ practice01/src/main/java/prob2/Prob2.java
@@ -11,6 +11,8 @@ public class Prob2 {
 		 * 
 		 * }
 		 */
+
+		/* 출력 값이 다음과 같도록 Prob2 클래스의 main 메쏘드를 완성하십시오. */
 		for (int i = 1; i < 10; i++) {
 			for (int j = 0; j < 10; j++) {
 				System.out.print(i + j + " ");
