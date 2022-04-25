44f8bd447fd133e8a6a3faf2e98db2701f569f4d
JSB1012 <sbj15102@naver.com>
2022-04-25 22:36:59 +0900

    practice

--- practice01/src/main/java/prob1/Prob1.java
+++ practice01/src/main/java/prob1/Prob1.java
@@ -7,6 +7,9 @@ public class Prob1 {
 		Scanner scanner = new Scanner(System.in);
 
 		/* 코드 작성 */
+		/* java.util.Scanner 클래스를 이용하여 입력된 수가 3의 배수인지 판별하는 프로그램을 작성하세요. */
+		
+	
 		System.out.print("수를 입력하세요:");
 		int number = scanner.nextInt();
 
