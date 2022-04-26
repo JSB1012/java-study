ce895e3322f79b5a6c8df077f667ca5b81213908
JSB1012 <sbj15102@naver.com>
2022-04-26 19:38:32 +0900

    practice

--- practice02/src/main/java/prob03/Prob03.java
+++ practice02/src/main/java/prob03/Prob03.java
@@ -5,7 +5,7 @@ import java.util.Arrays;
 public class Prob03 {
 	public static void main(String args[]) {
 		
-		/* 문자 배열(char[])을 입력 받아, 공백을 ‘,’로 변환하는 메소드와 char[]을 입력 받아 출력하는 메소드를 만드세요. */
+		/* 문)문자 배열(char[])을 입력 받아, 공백을 ‘,’로 변환하는 메소드와 char[]을 입력 받아 출력하는 메소드를 만드세요. */
 		char c[] = { 'T', 'h', 'i', 's', ' ', 'i', 's', ' ', 'a', ' ', 'p', 'e', 'n', 'c', 'i', 'l', '.' };
 
 		// 원래 배열 원소 출력
