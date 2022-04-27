c382ea858e2e2e2efa39410855230bc6dca6f7be
JSB1012 <sbj15102@naver.com>
2022-04-27 10:36:23 +0900

    class 정의-필드

---
+++ chapter03/src/main/java/chapter03/GoodsApp.java
@@ -0,0 +1,15 @@
+package chapter03;
+
+public class GoodsApp {
+
+	public static void main(String[] args) {
+		Goods goods = new Goods();
+		
+		goods.setName("nikon");
+		goods.setPrice(40000);
+		goods.setCountSold(50);
+		goods.setCountStock(30);
+		
+	}
+
+}
