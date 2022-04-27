f5023c40e39ad19f266bca2f7b28998f7f0b5ea2
JSB1012 <sbj15102@naver.com>
2022-04-27 17:46:57 +0900

    doubleToint, concat -과제

--- chapter03/src/main/java/chapter03/ArrayUtil.java
+++ chapter03/src/main/java/chapter03/ArrayUtil.java
@@ -4,19 +4,26 @@ public class ArrayUtil {
 
 	public static double[] intToDouble(int[] a) {
 		double[] result = new double[a.length];
-		for(int i = 0; i < a.length; i++) {
+		for (int i = 0; i < a.length; i++) {
 			result[i] = a[i];
 		}
-		
+
 		return result;
 	}
 
 	public static int[] doubleToint(double[] ds) {
-		return null;
+		int[] result2 = new int[ds.length];
+		for (int i = 0; i < ds.length; i++) {
+			result2[i] = (int) ds[i];
+		}
+		return result2;
 	}
 
 	public static int[] concat(int[] is1, int[] is2) {
-		return null;
+		int[] result3 = new int[is1.length + is2.length];
+		System.arraycopy(is1, 0, result3, 0, is1.length);
+		System.arraycopy(is2, 0, result3, is1.length, is2.length);
+		return result3;
 	}
 
 }
