9af8c8c5e141e9d6b6f84857e8062b414dc4ba4c
JSB1012 <sbj15102@naver.com>
2022-04-28 12:19:15 +0900

    클래스 정의 - final

--- chapter03/src/main/java/chapter03/Goods.java
+++ chapter03/src/main/java/chapter03/Goods.java
@@ -1,29 +1,41 @@
 package chapter03;
 
 public class Goods {
-	
-	public static int countOfGoods;		//main 앞에 static 붙이는 이유?
+	public static int countOfGoods;
 	private String name;
 	private int price;
 	private int countStock;
 	private int countSold;
 	
-	public Goods() {
-		Goods.countOfGoods = countOfGoods + 1;
+	public Goods(String name) {
+		// countOfGoods = countOfGoods + 1;
+		// this.name = name;
+		this(name, 0, 0, 0);
 	}
-
+	
+	public Goods() {
+		// countOfGoods = countOfGoods + 1;
+		this(null, 0, 0, 0);
+	}
+	
+	public Goods(String name, int price, int countStock, int countSold) {
+		countOfGoods = countOfGoods + 1;
+		
+		this.name = name;
+		this.price = price;
+		this.countSold = countStock;
+		this.countSold = countSold;
+	}
+	
 	public String getName() {
 		return name;
 	}
-
 	public void setName(String name) {
 		this.name = name;
 	}
-
 	public int getPrice() {
 		return price;
 	}
-
 	public void setPrice(int price) {
 		// 데이터 보호
 		if(price < 0) {
@@ -31,31 +43,30 @@ public class Goods {
 		}
 		this.price = price;
 	}
-
 	public int getCountStock() {
 		return countStock;
 	}
-
 	public void setCountStock(int countStock) {
 		this.countStock = countStock;
 	}
-
 	public int getCountSold() {
 		return countSold;
 	}
-
 	public void setCountSold(int countSold) {
 		this.countSold = countSold;
-	}					//private 참조 가능하게 getter/setter
+	}
 
 	public void showInfo() {
 		System.out.println(
-				"name:" + name + ",price:" + price + ", countStock:" + countStock + ", countSold:" + countSold);
-
+			"name:" + name + 
+			", price:" + price + 
+			", countStock:" + countStock + 
+			", countSold:" + countSold);
 	}
 
 	public int calcDiscountPrice(double discountRate) {
 		return (int)(discountRate * price);
 	}
 
+	
 }
--- chapter03/src/main/java/chapter03/GoodsApp.java
+++ chapter03/src/main/java/chapter03/GoodsApp.java
@@ -4,23 +4,25 @@ public class GoodsApp {
 
 	public static void main(String[] args) {
 		Goods goods = new Goods();
-		
+
 		goods.setName("nikon");
-		goods.setPrice(40000);
+		goods.setPrice(-1);
 		goods.setCountSold(50);
 		goods.setCountStock(30);
 		
 		goods.showInfo();
 		
-		Goods goods2 = new Goods();
+		Goods goods2 = new Goods("tv", 10000, 10, 10);
+		goods2.showInfo();
+		
 		Goods goods3 = new Goods();
-		System.out.println("Goods Count:" + Goods.countOfGoods);
+		goods3.showInfo();
+		
+		System.out.println("Goods Count:" + Goods.countOfGoods );
 		
 		// discount price 구하기
 		goods.setPrice(2000);
 		int discountPrice = goods.calcDiscountPrice(0.5);
 		System.out.println("Goods Discount Price:" + discountPrice);
-		
 	}
-
 }
