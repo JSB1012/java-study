8af114b53172ae2817e3984416bcf231047bde56
JSB1012 <sbj15102@naver.com>
2022-04-28 15:50:18 +0900

    instanceof 사용하기

--- chapter03/src/main/java/paint/Main.java
+++ chapter03/src/main/java/paint/Main.java
@@ -7,37 +7,60 @@ public class Main {
 		// point1.setY(10);
 		// point1.show();
 		Point point1 = new Point(20, 10);
-		drawPoint(point1);
+		draw(point1);
 
 		Point point2 = new Point(50, 100);
-		drawPoint(point2);
+		draw(point2);
 		point2.show(false);
 
 		Point point3 = new ColorPoint(40, 50, "red");
-		drawPoint(point3);
+		draw(point3);
 		// point3.show(true);
 
 		Triangle triangle = new Triangle();
-		drawTriangle(triangle);
+		draw(triangle);
 
 		Rectangle rectangle = new Rectangle();
-		drawRectangle(rectangle);
+		draw(rectangle);
+		
+		Circle circle = new Circle();
+		draw(circle);
+		
+		GraphicText graphicText = new GraphicText("Hellow World");
+		draw(graphicText);
+		
+		// instanceof 연산자 테스트
+		System.out.println(circle instanceof Object);
+		System.out.println(circle instanceof Shape);
+		System.out.println(circle instanceof Circle);
+		// 오류: class는 Hierachy 상위와 하위만 instanceof 연산자 사용 할 수 있음.
+		//System.out.println(circle instanceof Rectangle);
+		
+		// interface는 hierachy와 상관없이 instanceof 연산자를 사용 할 수 있다.
+		System.out.println(circle instanceof Drawable);
+		System.out.println(circle instanceof Runnable);
 	}
-
-	public static void drawPoint(Point point) {
-		point.show();
+	public static void draw(Drawable drawable) {
+		drawable.draw();
 	}
-
+//	public static void drawPoint(Point point) {
+//		point.show();
+//	}
+//
+//	
+//	public static void drawShape(Shape shape) {
+//		shape.draw();
+//	}
 //	public static void drawPoint(ColorPoint point) {
 //		point.show();
 //	}
 
-	public static void drawTriangle(Triangle triangle) {
-		triangle.draw();
-	}
-
-	public static void drawRectangle(Rectangle rectangle) {
-		rectangle.draw();
-	}
-
+//	public static void drawTriangle(Triangle triangle) {
+//		triangle.draw();
+//	}
+//
+//	public static void drawRectangle(Rectangle rectangle) {
+//		rectangle.draw();
+//	}
+	
 }
