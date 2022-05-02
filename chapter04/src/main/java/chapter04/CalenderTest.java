72efe7ded9f5c079df2fa551c4bb3abd0eddbaf6
JSB1012 <sbj15102@naver.com>
2022-05-02 12:02:14 +0900

    Calendar

--- chapter04/src/main/java/chapter04/CalenderTest.java
+++ chapter04/src/main/java/chapter04/CalenderTest.java
@@ -7,6 +7,30 @@ public class CalenderTest {
 	public static void main(String[] args) {
 
 		Calendar cal = Calendar.getInstance();
+		cal.set(Calendar.YEAR, 2022);
+		cal.set(Calendar.MONTH, 11); //12(Month +1)
+		cal.set(Calendar.DATE, 25);
+		
+		printDate(cal);
+		
+		cal.set(2022, 4, 2); // 2022/5/2
+		cal.add(Calendar.DATE, 100);
+		printDate(cal);
+
+	}
+
+	private static void printDate(Calendar cal) {
+		final String[] DAYS = {"일","월","화","수","목","금","토",}; //a에 10이 대입된게 여기가 마지막이다
+		int year = cal.get(Calendar.YEAR);
+		int month = cal.get(Calendar.MONTH); // 0~11; +1
+		int date = cal.get(Calendar.DATE);
+		int day = cal.get(Calendar.DAY_OF_WEEK); // 1(일) ~ 7(토)
+		int hours = cal.get(Calendar.HOUR);
+		int minutes = cal.get(Calendar.MINUTE);
+		int seconds = cal.get(Calendar.SECOND);
+
+		System.out.println(year + "년 " + (month + 1 < 10 ? "0" : "") + (month + 1) + "월 " + ((date < 10) ? "0" : "")
+				+ date + "일 " + DAYS[day-1] +"요일 "+ hours + "시 " + minutes + "분 " + seconds);
 
 	}
 
