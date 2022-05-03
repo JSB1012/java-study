2bea4f87810d56a3c864e7f27b50fd89e239300c
JSB1012 <sbj15102@naver.com>
2022-05-03 10:15:41 +0900

    Generic Method & 가변 파라미터 메소드

--- practice04/src/main/java/prob01/Printer.java
+++ practice04/src/main/java/prob01/Printer.java
@@ -1,25 +1,41 @@
 package prob01;
 
 public class Printer {
+//	public void println(int i) {
+//		System.out.println(i);
+//	}
+//	
+//	public void println(boolean b) {
+//		System.out.println(b);
+//	}
+//
+//	public void println(double d) {
+//		System.out.println(d);
+//	}
+//
+//	public void println(String s) {
+//		System.out.println(s);
+//	}
+	
+//	public <T> void println(T t) {
+//		System.out.println(t);
+//	}
 
-	public void println(int a) {
-
-		System.out.println(a);
+	public <T> void println(T... ts) {
+		for(T t : ts) {
+			System.out.print(t + " ");
+		}
+		
+		System.out.println("");
 	}
 
-	public void println(boolean b) {
-		System.out.println(b);
-
+	public int sum(int... nums) {
+		int sum = 0;
+		for(int n : nums) {
+			sum += n;
+		}
+		
+		return sum;
 	}
-
-	public void println(double c) {
-
-		System.out.println(c);
-	}
-
-	public void println(String d) {
-		System.out.println(d);
-
-	}
-
+	
 }
