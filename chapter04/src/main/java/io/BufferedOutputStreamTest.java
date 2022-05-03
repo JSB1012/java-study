55b2f1a54f95c5d6b4d55cf5e5830ba40313b843
JSB1012 <sbj15102@naver.com>
2022-05-03 15:26:17 +0900

    io #4

--- chapter04/src/main/java/io/BufferedOutputStreamTest.java
+++ chapter04/src/main/java/io/BufferedOutputStreamTest.java
@@ -9,24 +9,31 @@ public class BufferedOutputStreamTest {
 
 	public static void main(String[] args) {
 		BufferedOutputStream bos = null;
+		
 		try {
 			// 기반 스트림
 			FileOutputStream fos = new FileOutputStream("test.txt");
 		
 			// 보조 스트림
 			bos = new BufferedOutputStream(fos);
+			
+			// for(int i = 'a'; i <= 'z'; i++)
+			for(int i = 97; i <= 122; i++) {
+				bos.write(i);
+			}
 		} catch(FileNotFoundException ex) {
 			System.out.println("Can't Open:" + ex);
+		} catch(IOException ex) {
+			System.out.println("Error:" + ex);
 		} finally {
 			try {
 				if(bos != null) {
-				bos.close();
+					bos.close();
 				}
 			} catch (IOException e) {
 				e.printStackTrace();
 			}
 		}
-
 	}
 
 }
