9587544622c4a7f9eaa9b11dca666e509c3d6fd4
JSB1012 <sbj15102@naver.com>
2022-05-23 14:40:59 +0900

    java

--- practice04/src/main/java/prob03/Unit.java
+++ practice04/src/main/java/prob03/Unit.java
@@ -1,16 +1,16 @@
+
 package prob03;
 
-public abstract class Unit {
-	
-	/* 주의 사항 : 
-	 * 1. Unit 클래스를 새로 작성합니다.
-	 * 2. 기존 3개의 클래스는 문제의 조건에 맞게 수정 되어야 합니다.
-	 * 3. 메소드의 구체적 구현은 하지 않습니다.
-	 * 4. 실행되지 않고 클래스만 구현하세요.
-	 */
+public class Unit {
+	// 현재 위치
+	private int x;
+	private int y;
 
-	
-	public abstract void move();
-	public abstract void stop();
+	void move(int x, int y) {
+		/* 지정된 위치로 이동 */
+	}
 
+	void stop() {
+		/* 현재 위치에 정지 */
+	}
 }
